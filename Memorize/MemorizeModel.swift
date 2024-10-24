b8af8490e86d6ff7ac7033c034d58e2beee971dd
Bruno França do Prado <bruno_french@outlook.com>
2024-10-24 20:45:50 -0300

    feat: Finish Lecture 5 Stanford CS193p

--- Memorize/MemorizeModel.swift
+++ Memorize/MemorizeModel.swift
@@ -7,16 +7,24 @@
 
 import Foundation
 
-struct MemorizeModel<CardContent> {
+struct MemorizeModel<CardContent> where CardContent: Equatable {
     private(set) var cards: [Card]
+    var indexOfTheOneAndOnlyFaceUpCard: Int? {
+        get {
+            cards.indices.filter { index in cards[index].isFaceUp }.only
+        }
+        set {
+            cards.indices.forEach { cards[$0].isFaceUp = (newValue == $0) }
+        }
+    }
 
     init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
         cards = []
 
         for pairIndex in 0..<max(2, numberOfPairsOfCards) {
             let content = cardContentFactory(pairIndex)
-            cards.append(Card(content: content))
-            cards.append(Card(content: content))
+            cards.append(Card(id: "\(pairIndex+1)a", content: content))
+            cards.append(Card(id: "\(pairIndex+1)b", content: content))
         }
     }
 
@@ -24,13 +32,32 @@ struct MemorizeModel<CardContent> {
         cards.shuffle()
     }
 
-    func choose(_ card: Card) {
-
+    mutating func choose(_ card: Card) {
+        if let chosenIndex = cards.firstIndex(where: { $0.id == card.id }) {
+            if !cards[chosenIndex].isFaceUp && !cards[chosenIndex].isMatched {
+                if let potentialMatchIndex = indexOfTheOneAndOnlyFaceUpCard {
+                    if cards[chosenIndex].content == cards[potentialMatchIndex].content {
+                        cards[chosenIndex].isMatched = true
+                        cards[potentialMatchIndex].isMatched = true
+                    }
+                } else {
+                    indexOfTheOneAndOnlyFaceUpCard = chosenIndex
+                }
+                cards[chosenIndex].isFaceUp = true
+            }
+        }
     }
 
-    struct Card {
-        var isFaceUp = true
+    struct Card: Equatable, Identifiable {
+        var id: String
+        var isFaceUp = false
         var isMatched = false
         let content: CardContent
+    }
+}
+
+extension Array {
+    var only: Element? {
+        count == 1 ? first : nil
     }
 }
